b3bab68db793768be7c96dbaf29108746c1f949a
igsr5 <i@gsr5.dev>
2022-02-20 15:06:50 +0900

    Update s3 path

--- sync_images.sh
+++ sync_images.sh
@@ -4,4 +4,4 @@ LOG_OUT=./logs/stdout.log
 
 exec &> >(awk '{print strftime("[%Y/%m/%d %H:%M:%S] "),$0 } { fflush() } ' >> $LOG_OUT)
 
-aws s3 sync ./images s3://igsr5-images --delete
+aws s3 sync ./images $AWS_S3_BUCKET_PATH --delete
